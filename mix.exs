e29ca405c477f04a75ddf2b3a98e5ae10ab4da40
Filipe Cabaço <filipecabaco@gmail.com>
2024-09-20 12:33:15 +0100

    bump down to postgrex 0.18 for compatability reasons

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule PostgresReplication.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:postgrex, "~> 0.19.1"}
+      {:postgrex, "~> 0.18.0"}
     ]
   end
 end
